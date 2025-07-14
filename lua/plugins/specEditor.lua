450841844fcb5648d8c9da85d76c731184d77407
eduardosavian <104455792+eduardosavian@users.noreply.github.com>
2025-07-13 23:53:15 -0300

    Second commit

---
+++ lua/plugins/specEditor.lua
@@ -0,0 +1,14 @@
+return {
+
+	{
+	  "echasnovski/mini.icons",
+	  opts = {
+	    file = {
+	      [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+	    },
+	    filetype = {
+	      gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+	    },
+	  },
+	}
+}
